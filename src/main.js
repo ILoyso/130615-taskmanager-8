37360492af0fb38841436572cd3be8f1b1bb3fc1
Irina Shasheva <loyrina@gmail.com>
2019-02-23 19:07:40 +0300

    Create utils module, fix after tutors review

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import {generateRandomNumber} from './utils';
 import getFilterTemplate from './make-filter';
 import getTaskTemplate from './make-task';
 
+const MAX_NUMBER_OF_TASKS = 7;
+
 const filterContainer = document.querySelector(`.main__filter`);
 const taskContainer = document.querySelector(`.board__tasks`);
-const filtersData = [
+const filters = [
   {
     name: `All`,
     isChecked: true
@@ -28,14 +31,9 @@ const filtersData = [
   }
 ];
 
-const MAX_NUMBER_OF_TASKS = 7;
-
-// Random number - not including max value
-const generateRandomNumber = (max, min = 0) => Math.floor(Math.random() * (max - min)) + min;
-
-const createFiltersTemplate = (filters) => {
+const createFiltersTemplate = (filtersData) => {
   let filtersTemplate = ``;
-  for (const filter of filters) {
+  for (const filter of filtersData) {
     const isChecked = filter.isChecked;
     const randomNumber = isChecked ? generateRandomNumber(30, 1) : generateRandomNumber(30);
     filtersTemplate += getFilterTemplate(filter.name, randomNumber, isChecked);
@@ -43,8 +41,8 @@ const createFiltersTemplate = (filters) => {
   return filtersTemplate;
 };
 
-const renderFilters = (filters) => {
-  filterContainer.insertAdjacentHTML(`beforeend`, createFiltersTemplate(filters));
+const renderFilters = (filtersData) => {
+  filterContainer.insertAdjacentHTML(`beforeend`, createFiltersTemplate(filtersData));
 };
 
 const createTasksTemplate = (amount) => {
@@ -69,5 +67,5 @@ const onFilterClick = (evt) => {
 
 filterContainer.addEventListener(`click`, onFilterClick);
 
-renderFilters(filtersData);
+renderFilters(filters);
 renderTasks(MAX_NUMBER_OF_TASKS);
